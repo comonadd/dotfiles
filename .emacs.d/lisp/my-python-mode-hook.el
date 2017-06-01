b1f5b947a477d70c977d290ab473a5002a7c86f5
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-06-01 20:46:45 +0300

    Add the Python mode auto-formatting with autopep8
    The new auto-formatting is binded to <F8>. The autopep8
    package binary should be installed on system in order to
    use new format shortcut.

--- .emacs.d/lisp/my-python-mode-hook.el
+++ .emacs.d/lisp/my-python-mode-hook.el
@@ -3,9 +3,14 @@
 ;;;   This file defines all the hooks
 ;;; Code:
 
+(defun my/python-mode-bind-keys ()
+  "Bind keys for the Python mode."
+  (local-set-key (kbd "<f8>") 'py-autopep8))
+
 (defun my/python-mode-hook ()
   "Python mode hook."
-  (electric-indent-mode -1))
+  (electric-indent-mode -1)
+  (my/python-mode-bind-keys))
 
 (provide 'my-python-mode-hook)
 ;;; my-python-mode-hook.el ends here
