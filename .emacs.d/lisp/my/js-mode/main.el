507c2604c49102d06eedf0848df6e95362b531c7
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-06-09 09:42:10 +0300

    Fix the Emacs configuration JSX problems
    This commit removes the separate hook for JSX mode.
    It moves all the stuff related to the proper work
    of JSX mode to the JS mode directory.
    
    This commit also enables the global subword mode.

--- .emacs.d/lisp/my/js-mode/main.el
+++ .emacs.d/lisp/my/js-mode/main.el
@@ -2,17 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'flycheck)
 (require 'js-auto-beautify)
 (require 'web-beautify)
 (require 'js2-mode)
+(require 'web-mode)
 
 (defun my/js-mode/set-style ()
   "Set the style for the JavaScript mode."
-  (setq js-indent-level 2))
+  (setq js-indent-level 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2))
 
 (defun my/js-mode/bind-keys ()
   "Bind keys for the JavaScript mode."
-  (local-set-key (kbd "<f8>") 'web-beautify-js))
+  (local-set-key (kbd "<f8>")
+                 (lambda ()
+                   (interactive)
+                   (indent-region (point-min)
+                                  (point-max)))))
 
 (defun my/js-mode/add-hooks ()
   "Add hooks for the JavaScript mode."
@@ -25,12 +33,23 @@
   (my/js-mode/add-hooks)
   (js-auto-beautify-mode)
   (setq js2-highlight-level 3)
-  (js2-mode)
   (flycheck-mode t)
-  (subword-mode))
+  (message "enable subword mode")
+  (web-mode))
 
-;; Add the hook
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil)) ad-do-it) ad-do-it))
+
+;; Add the hooks
 (add-hook 'js-mode-hook 'my/js-mode/hook)
+(add-hook 'js2-mode-hook 'my/js-mode/hook)
+(add-hook 'web-mode-hook (lambda ()
+                           ;; short circuit js mode and just do everything in jsx-mode
+                           (if (equal web-mode-content-type "javascript")
+                               (web-mode-set-content-type "jsx")
+                             (message "now set to: %s" web-mode-content-type))))
 
 (provide 'my/js-mode/main)
 ;;; main.el ends here
