7fa52b8e9379acf41b44882fa633e7d650a1d7b6
Dmitry Guzeev <dmitry.guzeev@yahoo.com>
2017-06-30 19:22:00 +0300

    Remove Flycheck C++ stupid setup part

--- .emacs.d/lisp/my/cpp-mode/util.el
+++ .emacs.d/lisp/my/cpp-mode/util.el
@@ -26,29 +26,6 @@ and open it in current window."
   (my/cpp-mode/find-corresponding-file)
   (other-window -1))
 
-(defun my/cpp-mode/setup-flycheck-project-include-paths ()
-  "Get the paths that we need to include in project environment."
-  (let ((root (projectile-project-root)))
-    (let ((include-paths (list root (concat root "/src")
-                               (concat root "/include"))))
-      (progn
-        (setq flycheck-gcc-include-path include-paths)
-        (setq flycheck-clang-include-path include-paths)))))
-
-(defun my/cpp-mode/flycheck-setup ()
-  "Setup the flycheck package."
-  (setq-default flycheck-disabled-checkers '(c/c++-clang))
-
-  ;; Include the project paths
-  (when (projectile-project-p)
-    (my/cpp-mode/setup-flycheck-project-include-paths))
-
-  ;; Set the flycheck additional arguments
-  (let ((args (split-string "-std=c++17")))
-    (progn
-      (setq flycheck-gcc-args args)
-      (setq flycheck-clang-args args))))
-
 (defun my/cpp-mode/set-style ()
   "Set the style."
   (c-add-style "custom" '("bsd" (indent-tabs-mode . nil)
